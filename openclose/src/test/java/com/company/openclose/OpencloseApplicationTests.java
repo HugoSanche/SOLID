bf065a55bce96e92c6c57a148670e8809533cc03
HugoSanche <hbmclaren@gmail.com>
2023-12-14 12:29:52 -0600

    Before apply open-close principle

---
+++ openclose/src/test/java/com/company/openclose/OpencloseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.openclose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencloseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
